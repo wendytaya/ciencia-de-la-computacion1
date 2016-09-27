6c3937ab60352275cd006326bddc90ead8ee89aa
wendytaya <wendy.taya@ucsp.edu.pe>
2016-09-26 23:13:31 -0500

    Add files via upload

---
+++ Chapter_5/Ejercicio5_3.c
@@ -0,0 +1,39 @@
+/*ejercicio 5.3 */
+#include <stdio.h>
+void strad (char *s, char *t);
+int main() {
+	char s[20]="123";
+	char t[5]="4567";
+	strad(s,t);
+	printf ("%s\n",s);
+	
+}
+void strad (char *k, char *l){
+	
+	while(*k !='\0')
+		  k++;
+	while ((*k =*l) != '\0'){
+		   k++;
+		   l++;
+	}
+	
+}
+
+
+/*#include <stdio.h>
+void strad(char k[], char l[]);
+int main() {
+char s[20]="123";
+char t[5]="4567";
+strad(s,t);
+printf ("%s\n",s);
+}
+
+void strad(char k[], char l[]){
+int i,j;
+i=j=0;
+while(k[i] !='\0')
+i++;
+while((k[i++] =l[j++]) != '\0');
+}
+*/
---
+++ Chapter_5/p_lower.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+char lower(char *c){
+		
+	if (*c>='A' && *c<='Z')
+		return *c+ 'a'-'A';
+	else
+		return *c;
+	
+	
+}
+int main() {
+	char c= {'B'};
+	printf("%c",lower(&c));
+	
+	return 0;
+}
+
+
+/*#include <stdio.h>
+#define funcion (c>='A' && c<='Z'? c+ 'a'-'A':c) 
+int main() {
+	int  c={'B'};
+	printf("%c",lower(c));
+}
+
+int lower(int c){
+	return funcion;
+}
+*/
